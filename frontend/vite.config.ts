64d6cc511d7af7d478f179e53ed15ff0a767d6cd
GregSQT <gregory.souquet@hotmail.fr>
2025-06-23 19:52:56 +0200

    Add files via upload

---
+++ frontend/vite.config.ts
@@ -0,0 +1,25 @@
+// frontend/vite.config.ts
+//
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+    '@': path.resolve(__dirname, 'src'),
+    '@roster': path.resolve(__dirname, 'src/roster'),
+    '@data': path.resolve(__dirname, 'src/data'),
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@images': path.resolve(__dirname, '../src/images'),
+    '@ai': path.resolve(__dirname, '../ai'),
+    },
+  },
+});
+
