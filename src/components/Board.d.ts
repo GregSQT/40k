3b3d2019e8ad6056272181735ecc59089b142981
GregSQT <gregory.souquet@hotmail.fr>
2025-06-23 21:24:56 +0200

    Delete src directory

--- src/components/Board.d.ts
+++
@@ -1,1 +0,0 @@
-export default function Board(): import("react").JSX.Element;
--- src/data/units.d.ts
+++
@@ -1,9 +0,0 @@
-export type Unit = {
-    id: number;
-    name: string;
-    col: number;
-    row: number;
-    color: number;
-    MOVE: number;
-};
-export declare const initialUnits: Unit[];
