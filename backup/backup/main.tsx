275720c9fa253409df137c46a61b1198545da7a8
GregSQT <9604@holbertonstudents.com>
2025-07-13 01:13:00 +0200

    cleanup

--- backup/backup/main.tsx
+++
@@ -1,9 +0,0 @@
-// frontend/src/main.tsx
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './Routes' 
-
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
