64369a117017c075d3d62296b09d3c3c009e8477
GregSQT <gregory.souquet@hotmail.fr>
2026-03-10 20:17:40 +0100

    suppression de code mort : App.tsx, HomePage, GamePage, GamePageLayout, GameController, GameBoard, GameStatus, ReplayPage, PlayerVsAIPage

--- frontend/src/components/GamePageLayout.tsx
+++
@@ -1,8 +0,0 @@
-//frontend/src/components/GamePageLayout.tsx
-
-import { GameController } from "./GameController";
-import "../App.css";
-
-export default function GamePageLayout() {
-  return <GameController />;
-}
--- frontend/src/pages/GamePage.tsx
+++
@@ -1,6 +0,0 @@
-﻿// frontend/src/pages/GamePage.tsx
-import GamePageLayout from "../components/GamePageLayout";
-
-export default function GamePage() {
-  return <GamePageLayout />;
-}
